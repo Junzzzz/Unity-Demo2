ff6276b9585db42ae92f4fb00ab8555ff425b1d1
Junzzzz <0v0.git@gmail.com>
2022-06-28 20:56:54 +0800

    Add fox crouching

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public Animator animator;
-    public float moveSpeed;
-    public float jumpForce;
-
     private static readonly int Running = Animator.StringToHash("running");
     private static readonly int Jumping = Animator.StringToHash("jumping");
     private static readonly int Failing = Animator.StringToHash("falling");
+    private static readonly int Crouching = Animator.StringToHash("crouching");
+
+    private Rigidbody2D _rb;
+    private BoxCollider2D _box;
+    private Animator _animator;
+
+    public LayerMask groundMask;
+    public float moveSpeed;
+    public float jumpForce;
+
+    private static int Cherry = 0;
+
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _box = GetComponent<BoxCollider2D>();
+        _animator = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -21,7 +35,7 @@ public class PlayerController : MonoBehaviour
     private void PlayerMove()
     {
         var horizontal = Input.GetAxis("Horizontal");
-        var velocity = rb.velocity;
+        var velocity = _rb.velocity;
 
         // 玩家移动 & 跳跃
         if (horizontal != 0)
@@ -31,29 +45,65 @@ public class PlayerController : MonoBehaviour
 
             transform.localScale = new Vector3(horizontalRaw, 1, 1);
             // 移动动画
-            animator.SetFloat(Running, Math.Abs(horizontal));
+            _animator.SetFloat(Running, Math.Abs(horizontal));
         }
 
-        var jump = animator.GetBool(Jumping);
-        var fall = animator.GetBool(Failing);
-        if (!jump && !fall && Input.GetButton("Jump"))
+        var jump = _animator.GetBool(Jumping);
+        var fall = _animator.GetBool(Failing);
+        var crouch = _animator.GetBool(Crouching);
+        if (!jump && !fall)
         {
-            velocity.y = jumpForce * Time.deltaTime;
-            // 跳跃状态切换
-            animator.SetBool(Jumping, true);
-        }
+            if (!crouch && Input.GetButton("Jump"))
+            {
+                velocity.y = jumpForce * Time.deltaTime;
+                // 跳跃状态切换
+                _animator.SetBool(Jumping, true);
+            }
+            else if (Input.GetButton("Crouch"))
+            {
+                // 降低碰撞体积
+                SwitchCrouch(true);
 
+                _animator.SetBool(Crouching, true);
+            }
+            else if (crouch && CanStandUp())
+            {
+                // 还原碰撞体积
+                SwitchCrouch(false);
+                _animator.SetBool(Crouching, false);
+            }
+        }
 
         if (jump && velocity.y < 0)
         {
-            animator.SetBool(Jumping, false);
-            animator.SetBool(Failing, true);
+            _animator.SetBool(Jumping, false);
+            _animator.SetBool(Failing, true);
         }
-        else if (fall && velocity.y == 0)
+        else if (fall && Math.Abs(velocity.y) < 0.001)
         {
-            animator.SetBool(Failing, false);
+            _animator.SetBool(Failing, false);
         }
 
-        rb.velocity = velocity;
+
+        _rb.velocity = velocity;
+    }
+
+    private bool CanStandUp()
+    {
+        return !Physics2D.OverlapBox(_box.bounds.center, _box.size, 0, groundMask);
+    }
+
+    private void SwitchCrouch(bool isCrouch)
+    {
+        _box.enabled = !isCrouch;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Collections"))
+        {
+            Destroy(collision.gameObject);
+            Cherry++;
+        }
     }
 }
